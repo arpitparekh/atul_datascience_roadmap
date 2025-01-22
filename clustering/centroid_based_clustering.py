10a880c42f5196c20579e8d9c425be5cb3c98881
arpitparekh <arpitparekh9@gmail.com>
2025-01-22 09:31:34 +0530

    centroid

--- clustering/centroid_based_clustering.py
+++ clustering/centroid_based_clustering.py
@@ -6,14 +6,14 @@ from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 # Generate synthetic data
 
-X,y = make_blobs(n_samples=300, centers=3,cluster_std=1 ,random_state=42)
+X,y = make_blobs(n_samples=300, centers=3,cluster_std=5 ,random_state=42)
 
 print(X)
 print()
 print(y)
 
 # now create a clusters from random data
-model = KMeans(n_clusters=3,random_state=42)
+model = KMeans(n_clusters=4,random_state=42)
 model.fit(X)
 
 predicted =  model.labels_
@@ -23,5 +23,8 @@ plt.subplot(1,2,1)
 plt.scatter(X[:,0],X[:,1],color="red")
 plt.subplot(1,2,2)
 plt.scatter(X[:,0],X[:,1],c=predicted,cmap='plasma')
+# plot the centrod also
+plt.scatter(model.cluster_centers_[:,0],model.cluster_centers_[:,1],color="black")
+
 
 plt.show()
