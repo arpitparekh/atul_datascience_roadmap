75558a0186946ed8dab689cf77652157fefdfdc6
arpitparekh <arpitparekh9@gmail.com>
2025-01-22 09:21:16 +0530

    blob clustering

---
+++ clustering/centroid_based_clustering.py
@@ -0,0 +1,27 @@
+# centroid based clustering
+# k-means
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+# Generate synthetic data
+
+X,y = make_blobs(n_samples=300, centers=3,cluster_std=1 ,random_state=42)
+
+print(X)
+print()
+print(y)
+
+# now create a clusters from random data
+model = KMeans(n_clusters=3,random_state=42)
+model.fit(X)
+
+predicted =  model.labels_
+print(predicted)
+
+plt.subplot(1,2,1)
+plt.scatter(X[:,0],X[:,1],color="red")
+plt.subplot(1,2,2)
+plt.scatter(X[:,0],X[:,1],c=predicted,cmap='plasma')
+
+plt.show()
